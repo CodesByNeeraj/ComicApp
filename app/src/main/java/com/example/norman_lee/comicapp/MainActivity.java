bcc5729597682a376867b0762188a27b8c2cf37b
Fredy <fredy_tantri@ECCLT2203802s-MacBook-Pro.local>
2024-03-18 09:51:18 +0800

    fix TODOs

--- app/src/main/java/com/example/norman_lee/comicapp/MainActivity.java
+++ app/src/main/java/com/example/norman_lee/comicapp/MainActivity.java
@@ -41,40 +41,14 @@ public class MainActivity extends AppCompatActivity {
 
         //TODO 6.1 Ensure that Android Manifest has permissions for internet and has orientation fixed
         //TODO 6.2 Get references to widgets
-        editTextComicNo = findViewById(R.id.editTextComicNo);
-        buttonGetComic = findViewById(R.id.buttonGetComic);
-        textViewTitle = findViewById(R.id.textViewTitle);
-        imageViewComic = findViewById(R.id.imageViewComic);
 
-        //TODO 6.3 - 6.4 and 6.15 **********************************
+        //TODO 6.3 - 6.5 **********************************
         //TODO 6.3 Set up setOnClickListener for the button
         //TODO 6.4 Retrieve the user input from the EditText
         //TODO 6.5 If network is active, call the getComic method. Otherwise, show a Toast message
-        buttonGetComic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String userInput = editTextComicNo.getText().toString();
-                if (Utils.isNetworkAvailable(MainActivity.this)) {
-                    getComic(userInput);
-                } else {
-                    Toast.makeText(MainActivity.this, "No Network", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+
 
         //TODO 6.6 - 6.15 Modify GetComic Below *************
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i("MainActivity", "onPause");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i("MainActivity", "onDestroy");
     }
 
     //TODO 6.6 - 6.15 ****************
@@ -89,49 +63,6 @@ public class MainActivity extends AppCompatActivity {
     //TODO 6.13 Extract the title with key "safe_title"
     //TODO 6.14 Download the Bitmap using Utils.getBitmap
     //TODO 6.15 (main thread) Assign the bitmap downloaded to imageView and set the title to textViewTitle
-
     void getComic(final String userInput) {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        Looper uiLooper = Looper.getMainLooper();
-        final Handler handler = new Handler(uiLooper);
-
-        executorService.execute(new Runnable() {
-            @Override
-            public void run() { // Background thread
-                final Container<Bitmap> cBitmap = new Container<>();
-                URL url = Utils.buildURL(userInput);
-                String response = Utils.getJson(url);
-                if (response == null) {
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            // main thread
-                            Log.i("UI Thread", "Invalid URL");
-                            Toast.makeText(MainActivity.this, "Invalid Comic No", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                } else {
-                    try {
-                        JSONObject jsonObject = new JSONObject(response);
-                        String imgURLString = jsonObject.getString("img");
-                        final String titleString = jsonObject.getString("safe_title");
-                        Bitmap bitmap = Utils.getBitmap(new URL(imgURLString));
-                        cBitmap.set(bitmap);
-                        handler.post(new Runnable() {
-                            // main thread
-                            @Override
-                            public void run() {
-                                imageViewComic.setImageBitmap(cBitmap.get());
-                                textViewTitle.setText(titleString);
-                            }
-                        });
-                    } catch (JSONException e) {
-                        throw new RuntimeException(e);
-                    } catch (MalformedURLException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-        });
     }
 }
